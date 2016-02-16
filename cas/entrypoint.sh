0d0fc86cec84fe608cf737ec433b67beaaf13bdc
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-16 15:32:48 +0100

    Update entrypoint.sh

--- cas/entrypoint.sh
+++ cas/entrypoint.sh
@@ -5,4 +5,5 @@ set -e
 if [ -e "/var/lib/georchestra/cas.war" ] ; then
   rm -rf /usr/local/tomcat/webapps/*
   cp /var/lib/georchestra/cas.war /usr/local/tomcat/webapps
+  chmod a+r /usr/local/tomcat/webapps/*.war
 fi
