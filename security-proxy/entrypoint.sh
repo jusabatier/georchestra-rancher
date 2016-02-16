4535bdbd58dbfdbfb7f86e49f13e2cb8ffd42d17
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-16 15:32:35 +0100

    Update entrypoint.sh

--- security-proxy/entrypoint.sh
+++ security-proxy/entrypoint.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-cp /imports/wars/ROOT.war /usr/local/tomcat/webapps/ROOT.war
-chmod a+r /usr/local/tomcat/webapps/*.war
+if [ -e "/var/lib/georchestra/ROOT.war" ] ; then
+  rm /usr/local/tomcat/webapps/*
+  cp /var/lib/georchestra/ROOT.war /usr/local/tomcat/webapps/ROOT.war
+  chmod a+r /usr/local/tomcat/webapps/*.war
+fi
