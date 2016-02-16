2fc057174c417e2c774a8c36b276b3ff3f166735
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-16 10:22:35 +0100

    Update entrypoint.sh

--- cas/entrypoint.sh
+++ cas/entrypoint.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+rm -rf /usr/local/tomcat/logs
+ln -s /imports/logs/cas /usr/local/tomcat/logs
 cp /imports/wars/cas.war /usr/local/tomcat/webapps/cas.war
 chmod a+r /usr/local/tomcat/webapps/*.war
