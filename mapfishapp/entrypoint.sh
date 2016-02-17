23ed26afa934682fd5acec0c4f6f475c7884b0e2
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-17 10:41:15 +0100

    Create entrypoint.sh

---
+++ mapfishapp/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+if [ -e "/var/lib/georchestra/mapfishapp.war" ] ; then
+  rm -rf /usr/local/tomcat/webapps/*
+  cp /var/lib/georchestra/mapfishapp.war /usr/local/tomcat/webapps
+  chmod a+r /usr/local/tomcat/webapps/*.war
+fi
+
+/usr/local/tomcat/bin/catalina.sh run
