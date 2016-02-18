8dae083e8ca73bd4452cd4b9542e8c35dc6108da
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-18 09:58:05 +0100

    Create entrypoint.sh

---
+++ cadastrapp/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -e "/var/lib/georchestra/cas.war" ] ; then
+  rm -rf /usr/local/tomcat/webapps/*
+  cp /var/lib/georchestra/cas.war /usr/local/tomcat/webapps
+  chmod a+r /usr/local/tomcat/webapps/*.war
+fi
+
+if [ -e "/var/lib/georchestra/tc-cadastrapp/server.xml" ] ; then
+  cp /var/lib/georchestra/tc-cadastrapp/server.xml /usr/local/tomcat/conf/
+  chmod a+r /usr/local/tomcat/conf/server.xml
+fi
+
+if [ -e "/var/lib/georchestra/tc-cadastrapp/context.xml" ] ; then
+  cp /var/lib/georchestra/tc-cadastrapp/context.xml /usr/local/tomcat/conf/
+  chmod a+r /usr/local/tomcat/conf/context.xml
+fi
+
+/usr/local/tomcat/bin/catalina.sh run
