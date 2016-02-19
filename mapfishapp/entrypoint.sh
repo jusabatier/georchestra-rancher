6aa13d6ce2627ae05742a294bcb94d9f0a42547e
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2016-02-19 09:27:07 +0100

    Update entrypoint.sh

--- mapfishapp/entrypoint.sh
+++ mapfishapp/entrypoint.sh
@@ -6,6 +6,10 @@ if [ -e "/var/lib/georchestra/mapfishapp.war" ] ; then
   rm -rf /usr/local/tomcat/webapps/*
   cp /var/lib/georchestra/mapfishapp.war /usr/local/tomcat/webapps
   chmod a+r /usr/local/tomcat/webapps/*.war
+  mkdir -p /usr/local/tomcat/webapps/mapfishapp
+  unzip -e /usr/local/tomcat/webapps/mapfishapp.war -d /usr/local/tomcat/webapps/mapfishapp
+  rm -rf /usr/local/tomcat/webapps/mapfishapp/WEB-INF/lib/imageio-ext-gdalframework-*.jar
+  cp /usr/share/java/gdal.jar /usr/local/tomcat/webapps/mapfishapp/WEB-INF/lib/gdal.jar
 fi
 
 /usr/local/tomcat/bin/catalina.sh run
