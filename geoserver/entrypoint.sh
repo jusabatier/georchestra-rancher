c738405f86baed3f1b4255ab0f894101bf7837cd
Julien Sabatier <jusabatier@devgraph>
2016-03-23 08:52:27 +0100

    Add geoserver image

---
+++ geoserver/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ -e "/var/lib/georchestra/geoserver.war" ] ; then
+  rm -rf /usr/local/tomcat/webapps/*
+  cp /var/lib/georchestra/geoserver.war /usr/local/tomcat/webapps
+  chmod a+r /usr/local/tomcat/webapps/*.war
+  mkdir -p /usr/local/tomcat/webapps/geoserver
+  unzip -e /usr/local/tomcat/webapps/geoserver.war -d /usr/local/tomcat/webapps/geoserver
+  rm -rf /usr/local/tomcat/webapps/geoserver/WEB-INF/lib/imageio-ext-gdal-bindings-*.jar
+  cp /usr/share/java/gdal.jar /usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gdal.jar
+fi
+
+/usr/local/tomcat/bin/catalina.sh run
